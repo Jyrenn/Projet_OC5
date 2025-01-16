690dbf4dcf4ef4acbce1e656cfe1f6ae5f82a285
Jyrenn <clement.martin.27@gmail.com>
2025-01-16 15:38:58 +0100

    Peauffinages banner.jsx

--- Desktop/Projet05_OC/my-react-app/src/components/banner.jsx
+++ Desktop/Projet05_OC/my-react-app/src/components/banner.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import image from "../assets/Images/eric-muhr-P_XxsdVgtpQ-unsplash.png";
+import defaultImage from "../assets/Images/eric-muhr-P_XxsdVgtpQ-unsplash.png";
 import "../scss/banner.scss";
 
-function Banner() {
+const Banner = ({ image = defaultImage }) => {
   return (
     <div className="banner">
       <img src={image} alt="photo paysage" />
       <h1>Chez vous, partout et ailleurs</h1>
     </div>
   );
-}
+};
 export default Banner;
